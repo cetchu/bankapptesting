a868305c16d36a8443f4111bf7644a8cf78b9805
gitbanking <clinton.etchu@gmail.com>
2021-01-07 18:19:51 -0500

    commit bank

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello wold");
+	System.out.println("hello world");
+	System.out.println("to test check");
     }
 }
